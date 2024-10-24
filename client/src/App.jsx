05dba794af6bc5f33b0bd0f61c9dfbcd9f501961
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-10-24 23:55:15 +0530

    Add Header.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Footer.jsx";
 import "@fortawesome/fontawesome-free/css/all.css";
+import Header from "./Header.jsx";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/footer" element={<Footer/>}></Route>
+        <Route path="/footer" element={<Footer />}></Route>
+        <Route path="/header" element={<Header />}></Route>
       </Routes>
     </BrowserRouter>
   );
