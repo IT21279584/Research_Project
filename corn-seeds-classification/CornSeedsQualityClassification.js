c8d4952373c1971f2a6a51bb3ec319ccb2adc95e
IT21279584 <it21279584@my.sliit.lk>
2024-10-31 01:13:22 +0530

    update corn model

--- corn-seeds-classification/CornSeedsQualityClassification.js
+++ corn-seeds-classification/CornSeedsQualityClassification.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const CornSeedsQualityClassificationSchema = new mongoose.Schema({
-  imageNames: [String], // Store multiple image names
-  classifications: [String], // Store multiple classifications
-  imageDatas: [Buffer], // Store an array of images as Buffers
-  date: { type: Date, default: Date.now }
+  imageUrls: [String], // Store S3 image URLs
+  classifications: [String], // Store classification results
+  date: { type: Date, default: Date.now }, // Store the timestamp
 });
 
 module.exports = mongoose.model(
