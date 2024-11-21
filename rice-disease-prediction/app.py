be560ecebc2c6a4e34937a1135360e2beecd7087
IT21338748 <it21279584@my.sliit.lk>
2024-11-22 01:18:30 +0530

    update python file

--- rice-disease-prediction/app.py
+++ rice-disease-prediction/app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 # Load the pre-trained model once when the app starts
-model_path = os.path.join("models", "efficientnet_soybean_final_model.keras")
+model_path = os.path.join("models", "resnet_rice_final_model.keras")
 model = tf.keras.models.load_model(model_path)
 
 # Define your labels in the correct order based on the model's training data
@@ -17,7 +17,7 @@ labels = ["Bacterial Leaf Blight", "Brown Spot", "Healthy","Leaf Blast"]
 def preprocess_image(image_bytes):
     # Preprocess the uploaded image to match model input shape and requirements
     img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    img = img.resize((227, 227))  # Resize to match model's expected input size
+    img = img.resize((224, 224))  # Resize to match model's expected input size
     img = np.expand_dims(img, axis=0)  # Add batch dimension
     return img
 
