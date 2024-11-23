fe6377f5e5c099afa18256b2e47e1878ae787512
it21333552 <it21333552@my.sliit.lk>
2024-11-23 22:59:59 +0530

    add prediction result

---
+++ guava-quality-classification/express-service/models/PredictionResult.js
@@ -0,0 +1,16 @@
+// models/PredictionResult.js
+const mongoose = require('mongoose');
+
+const predictionResultSchema = new mongoose.Schema({
+    predicted_class: String,
+    average_confidence: Number,
+    probabilities: {
+        high: Number,
+        medium: Number,
+        low: Number
+    },
+    image1: String,  // Path or URL of the first image
+    image2: String   // Path or URL of the second image
+}, { timestamps: true });
+
+module.exports = mongoose.model('PredictionResultGuava', predictionResultSchema);
