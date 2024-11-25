be15899ce9ffafdde96687bfce8de1baf11731c6
it21338748 <it21338748@my.sliit.lk>
2024-11-25 10:00:00 +0530

    Update app.py

--- rice-disease-prediction/flask-service/app.py
+++ rice-disease-prediction/flask-service/app.py
@@ -11,3 +11,14 @@ app = Flask(__name__)
 model_path = os.path.join("models", "rice_disease.keras")
 model = tf.keras.models.load_model(model_path)
 
+# Define your labels in the correct order based on the model's training data set
+labels = ["Bacterial Leaf Blight", "Brown Spot", "Healthy","Leaf Blast"]
+
+def preprocess_image(image_bytes):
+    # Preprocess the uploaded image to match model input shape and requirements
+    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    img = img.resize((224, 224))  # Resize to match model's expected input size
+    img = np.expand_dims(img, axis=0)  # Adding batch dimension
+    return img
+
+
