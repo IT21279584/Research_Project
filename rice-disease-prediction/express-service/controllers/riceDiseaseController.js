ab40a4c0d49ef801a53b57203978dd2f08105cfa
it21338748 <it21338748@my.sliit.lk>
2024-11-26 10:00:00 +0530

    Update controllers

--- rice-disease-prediction/express-service/controllers/riceDiseaseController.js
+++ rice-disease-prediction/express-service/controllers/riceDiseaseController.js
@@ -8,44 +8,7 @@ const riceDiseasePrediction = async (req, res) => {
       return res.status(400).json({ message: "Please upload exactly 1 image" });
     }
 
-    const file = req.files[0];
-    const imageKey = `image-${Date.now()}-${file.originalname}`;
-    console.log(`Uploading image to S3: ${imageKey}`);
-
-    const imageUrl = await uploadToS3(file, imageKey);
-
-    const prediction = await classifyImage(file);
-
-    const record = new Classification({
-      imageUrls: [imageUrl],
-      predictions: [prediction],
-    });
-
-    await record.save();
-    res.json({ prediction: [prediction] });
-  } catch (err) {
-    console.error("Error with prediction:", err.message);
-    res
-      .status(500)
-      .json({ message: "Internal server error", error: err.message });
-  }
-};
-
-const getPreviousPredictions = async (req, res) => {
-  try {
-    const lastRecords = await Classification.find().sort({ date: -1 }).limit(4);
-    const formattedResults = lastRecords.map((record) => ({
-      images: record.imageUrls || [],
-      predictions: record.predictions || [],
-      date: record.date || null,
-    }));
-    res.json(formattedResults);
-  } catch (err) {
-    console.error("Error fetching previous results:", err.message);
-    res
-      .status(500)
-      .json({ message: "Error fetching previous results", error: err.message });
-  }
+    
 };
 
 module.exports = { riceDiseasePrediction, getPreviousPredictions };
