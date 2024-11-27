f3678acc9b220831969e55eed79c2c90bf56eb09
it21338748 <it21338748@my.sliit.lk>
2024-11-27 10:00:00 +0530

    Update routes

--- rice-disease-prediction/express-service/routes/riceDiseaseRoutes.js
+++ rice-disease-prediction/express-service/routes/riceDiseaseRoutes.js
@@ -5,7 +5,13 @@ const {
   getPreviousPredictions,
 } = require("../controllers/riceDiseaseController");
 
+const router = express.Router();
 
+router.post(
+  "/rice-disease-predictions",
+  upload.array("images", 1),
+  riceDiseasePrediction
+);
 router.get("/rice-previous-predictions", getPreviousPredictions);
 
 module.exports = router;
