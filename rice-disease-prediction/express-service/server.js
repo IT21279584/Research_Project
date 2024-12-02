e687e1fa595b509050ccc973a9853270b911d860
it21338748 <it21338748@my.sliit.lk>
2024-12-02 10:00:00 +0530

    update server.js

--- rice-disease-prediction/express-service/server.js
+++ rice-disease-prediction/express-service/server.js
@@ -8,15 +8,7 @@ const riceDiseaseRoutes = require("./routes/riceDiseaseRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configure Middleware
-app.use(cors());
-app.use(express.json());
 
-// MongoDB Configuration
-mongooseConfig();
-
-// API Routes
-app.use("/api", riceDiseaseRoutes);
 
 // Start Express Server
 app.listen(PORT, () => {
