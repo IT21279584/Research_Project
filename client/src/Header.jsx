5041e56dfeae7fb2ea41bac80e2b0205c0ac6a07
IT21338748 <it21279584@my.sliit.lk>
2025-01-26 00:19:37 +0530

    Update header logo with routes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,6 +1,13 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faShoppingCart, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faShoppingCart,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link as ScrollLink } from "react-scroll"; // Import Link from react-scroll
+import logo from "./assets/logo.png";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,12 +17,13 @@ const Header = () => {
   };
 
   return (
-    <header className="py-4 bg-white shadow-sm font-archivo">
+    <header className="py-1 bg-white shadow-sm font-archivo">
       <div className="container flex items-center justify-between px-4 mx-auto">
         {/* Logo (Hidden on small screens, visible on md and larger) */}
-        <div className="hidden text-xl font-bold md:block">
-          <a href="#" className="text-gray-900">
-            Brand
+        <div className="hidden md:block">
+          <a href="/" className="text-gray-900">
+            <img src={logo} alt="Farm Guard" className="w-32" />{" "}
+            {/* Set width for the logo */}
           </a>
         </div>
 
@@ -39,33 +47,48 @@ const Header = () => {
             menuOpen ? "block" : "hidden"
           } md:block absolute md:static top-16 left-0 w-full md:w-auto bg-white md:bg-transparent z-20`}
         >
+          {/* Home Link */}
           <a
             href="/"
-            className="block py-2 text-gray-600 md:inline hover:text-black md:py-0"
+            className="block py-2 text-gray-600 cursor-pointer md:inline hover:text-black md:py-0"
           >
             Home
           </a>
-          <a
-            href="#"
-            className="block py-2 text-gray-600 md:inline hover:text-black md:py-0"
+
+          {/* Services Link (With smooth scrolling) */}
+          <ScrollLink
+            to="services" // Link to section with id="services"
+            smooth={true}
+            duration={500}
+            className="block py-2 text-gray-600 cursor-pointer md:inline hover:text-black md:py-0"
           >
             Services
-          </a>
-          <a
-            href="#"
-            className="block py-2 text-gray-600 md:inline hover:text-black md:py-0"
+          </ScrollLink>
+
+          {/* News Link (With smooth scrolling) */}
+          <ScrollLink
+            to="news" // Link to section with id="news"
+            smooth={true}
+            duration={500}
+            className="block py-2 text-gray-600 cursor-pointer md:inline hover:text-black md:py-0"
           >
             News
-          </a>
-          <a
-            href="#"
-            className="block py-2 text-gray-600 md:inline hover:text-black md:py-0"
+          </ScrollLink>
+
+          {/* Community Link (With smooth scrolling) */}
+          <ScrollLink
+            to="community" // Link to section with id="community"
+            smooth={true}
+            duration={500}
+            className="block py-2 text-gray-600 cursor-pointer md:inline hover:text-black md:py-0"
           >
             Community
-          </a>
+          </ScrollLink>
+
+          {/* About Us Link  */}
           <a
-            href="#"
-            className="block py-2 text-gray-600 md:inline hover:text-black md:py-0"
+            href="#about-us"
+            className="block py-2 text-gray-600 cursor-pointer md:inline hover:text-black md:py-0"
           >
             About Us
           </a>
@@ -80,16 +103,16 @@ const Header = () => {
 
           {/* Login Button */}
           <a
-            href="#"
-            className="px-4 py-2 text-gray-600 border border-green-500 rounded-lg hover:bg-green-500 hover:text-white"
+            href="/sign-in"
+            className="px-4 py-2 text-gray-600 border border-green-800 rounded-lg hover:bg-green-800 hover:text-white"
           >
             Login
           </a>
 
           {/* Signup Button */}
           <a
-            href="#"
-            className="px-4 py-2 text-white bg-green-500 border border-green-500 rounded-lg hover:bg-white hover:text-gray-500"
+            href="/sign-up"
+            className="px-4 py-2 text-white bg-green-800 border border-green-800 rounded-lg hover:bg-white hover:text-gray-500"
           >
             Register
           </a>
