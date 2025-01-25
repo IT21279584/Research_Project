2e87e515b62c24a8c58f5e50a2dfb196ad060de1
IT21338748 <it21279584@my.sliit.lk>
2025-01-26 00:18:55 +0530

    Update footer design

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,157 +1,95 @@
 import React from "react";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { faPhone, faEnvelope, faMapMarkerAlt, faPaperPlane } from '@fortawesome/free-solid-svg-icons'; // Importing faPaperPlane
+import logo from "./assets/logo-removebg.png";
 
 const Footer = () => {
   return (
-    <footer className="pt-12 pb-6 text-gray-400 bg-gray-900 font-archivo">
-      <div className="container grid grid-cols-1 gap-8 px-6 mx-auto md:px-12 md:grid-cols-4">
-        {/* Logo & Description */}
-        <div>
-          <h2 className="mb-4 text-2xl font-bold text-white">Agrios</h2>
-          <p className="mb-6">
-            There are many variations of passages of lorem ipsum available, but
-            the majority suffered.
-          </p>
-          <div className="flex space-x-4">
-            <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-pinterest"></i>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-instagram"></i>
-            </a>
+    <footer className="px-4 py-12 text-white bg-green-800">
+      <div className="grid grid-cols-1 gap-8 mx-auto max-w-7xl md:grid-cols-3">
+        {/* Left Section with Logo and Subscribe */}
+        <div className="flex flex-col items-start md:col-span-1">
+          <div className="flex items-center mb-4">
+            <img
+              src={logo}
+              alt="Farm Guard Logo"
+              className="mr-3 w-72" // Set a desired width
+            />
           </div>
-        </div>
 
-        {/* Explore */}
-        <div>
-          <h2 className="relative pb-5 mb-4 font-bold text-white">
-            Explore
-            <span className="absolute bottom-0 left-0 w-6 h-1 bg-green-500 "></span>
-            <span className="absolute bottom-0 w-1 h-1 ml-2 bg-green-500 rounded-full left-6"></span>
-          </h2>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:text-white">
-                <i className="mr-2 fa-solid fa-leaf"></i>
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-white">
-                <i className="mr-2 fa-solid fa-leaf"></i>
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-white">
-                <i className="mr-2 fa-solid fa-leaf"></i>
-                Our Projects
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-white">
-                <i className="mr-2 fa-solid fa-leaf"></i>
-                Meet the Farmers
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-white">
-                <i className="mr-2 fa-solid fa-leaf"></i>
-                Latest News
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-white">
-                <i className="mr-2 fa-solid fa-leaf"></i>
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        {/* News */}
-        <div>
-          <h2 className="relative pb-5 mb-4 font-bold text-white">
-            News
-            <span className="absolute bottom-0 left-0 w-6 h-1 bg-green-500 "></span>
-            <span className="absolute bottom-0 w-1 h-1 ml-2 bg-green-500 rounded-full left-6"></span>
-          </h2>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:text-white">
-                Bringing Food Production Back To Cities
-              </a>
-              <p className="text-sm">July 5, 2022</p>
-            </li>
-            <li>
-              <a href="#" className="hover:text-white">
-                The Future of Farming, Smart Irrigation Solutions
-              </a>
-              <p className="text-sm">July 5, 2022</p>
-            </li>
-          </ul>
-        </div>
-
-        {/* Contact */}
-        <div>
-          <h2 className="relative pb-5 mb-4 font-bold text-white">
-            Contact
-            <span className="absolute bottom-0 left-0 w-6 h-1 bg-green-500 "></span>
-            <span className="absolute bottom-0 w-1 h-1 ml-2 bg-green-500 rounded-full left-6"></span>
-          </h2>
-          <ul className="space-y-2">
-            <li className="flex items-center space-x-2">
-              <i className="fas fa-phone"></i>
-              <span>666 888 0000</span>
-            </li>
-            <li className="flex items-center space-x-2">
-              <i className="fas fa-envelope"></i>
-              <span>needhelp@company.com</span>
-            </li>
-            <li className="flex items-center space-x-2">
-              <i className="fas fa-map-marker-alt"></i>
-              <span>80 Brooklyn Golden Street Line, New York, USA</span>
-            </li>
-          </ul>
-          <div className="mt-4">
-            <div className="flex">
+          <div className="w-full">
+            <p className="mb-2">Subscribe to our channel</p>
+            <div className="relative">
               <input
                 type="email"
-                placeholder="Your Email Address"
-                className="flex-grow p-4 text-white bg-gray-800 rounded-l-md"
+                placeholder="Email"
+                className="w-full px-10 py-2 text-gray-900 placeholder-white bg-gray-600 rounded-md focus:outline-none"
               />
-              <button className="p-4 text-white bg-green-500 hover:bg-green-600 rounded-r-md">
-                <FontAwesomeIcon icon={faPaperPlane} />{" "}
-              </button>
+              <span className="absolute inset-y-0 flex items-center text-white left-3">
+                <i className="fas fa-envelope"></i> {/* Email icon in white */}
+              </span>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Bottom section */}
-      <div className="pt-6 mt-12 text-sm text-center border-t border-gray-700">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          {/* All Rights Reserved */}
-          <p className="text-cente">© All Copyright 2024 by shawonetc Themes</p>
-
-          {/* Terms of Use and Privacy Policy */}
-          <div className="justify-center mt-4 space-x-4 md:justify-center md:mt-0">
-            <a href="#" className="hover:text-white">
-              Terms of Use
-            </a>
-            <span>|</span>
-            <a href="#" className="hover:text-white">
-              Privacy Policy
-            </a>
+        {/* Right Section with Paragraph and Links */}
+        <div className="text-left md:col-span-2">
+          <p className="mb-4 leading-relaxed text-m">
+            Discover a premier AI-based platform offering comprehensive farming
+            experience. With a rich heritage of over thousands of years in
+            traditional farming techniques, this platform provides a combination
+            of the latest technology with traditional farming worldwide. From
+            large-scale farms and factories to small fields of crops, our
+            platform provides services for disease management, quality
+            assessment, and more farming sectors.
+          </p>
+          <div className="flex justify-between text-left">
+            <div>
+              <h2 className="mb-2 text-lg font-semibold">Services</h2>
+              <ul className="space-y-2">
+                <li>
+                  <a href="#" className="text-gray-400 hover:underline">
+                    FAQs
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-semibold">News</h2>
+              <ul className="space-y-2">
+                <li>
+                  <a href="#" className="text-gray-400 hover:underline">
+                    Trips
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-gray-400 hover:underline">
+                    News
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-semibold">Community</h2>
+              <ul className="space-y-2">
+                <li>
+                  <a href="#" className="text-gray-400 hover:underline">
+                    Vlogs
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-gray-400 hover:underline">
+                    Events
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-gray-400 hover:underline">
+                    Feedbacks
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-semibold">About Us</h2>
+            </div>
           </div>
         </div>
       </div>
