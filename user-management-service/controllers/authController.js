b02c1f0b8384fb36910cc9cf4c784a2e2e14d95a
IT21338748 <it21279584@my.sliit.lk>
2025-01-29 00:00:15 +0530

    add auth controller

---
+++ user-management-service/controllers/authController.js
@@ -0,0 +1,49 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils/jwtUtils");
+
+// User Registration
+const registerUser = async (req, res) => {
+  const { name, email, password, country } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const user = await User.create({ name, email, password, country });
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user", error });
+  }
+};
+
+// User Login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error });
+  }
+};
+
+module.exports = { registerUser, loginUser };
