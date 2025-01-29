ede0bd13c0dc243b8e5a71a81394f8bba11fdb86
IT21338748 <it21279584@my.sliit.lk>
2025-01-30 00:23:35 +0530

    update model

--- user-management-service/models/User.js
+++ user-management-service/models/User.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  country: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String },
+    country: { type: String },
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
