5c6d47cab5b4eb8d730a16fb9272e2ac68aa205d
IT21338748 <it21279584@my.sliit.lk>
2025-02-01 01:19:10 +0530

    Remove multers3 and update code

--- user-management-service/config/s3.js
+++ user-management-service/config/s3.js
@@ -1,28 +1,28 @@
-    const AWS = require("aws-sdk");
-    const multer = require("multer");
-const multerS3 = require("multer-s3");
-    require("dotenv").config();
+// config/s3Config.js
+const AWS = require("aws-sdk");
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
-    AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
-    });
+// Function to upload a file to S3
+const uploadToS3 = async (file, imageKey) => {
+  try {
+    const uploadResult = await s3
+      .upload({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: imageKey,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      })
+      .promise();
 
-    const s3 = new AWS.S3();
+    return uploadResult.Location; // Return the S3 URL of the uploaded file
+  } catch (err) {
+    throw new Error("Error uploading image to S3: " + err.message);
+  }
+};
 
-    const upload = multer({
-      storage: multerS3({
-        s3,
-        bucket: process.env.AWS_BUCKET_NAME,
-        metadata: (req, file, cb) => {
-          cb(null, { fieldName: file.fieldname });
-        },
-        key: (req, file, cb) => {
-          cb(null, `profile_pictures/${Date.now()}_${file.originalname}`);
-        },
-      }),
-    });
-
-    module.exports = upload;
+module.exports = { s3, uploadToS3 };
