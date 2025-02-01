bd611be85a427f79abc904de535fa1c9dabf2f4e
IT21338748 <it21279584@my.sliit.lk>
2025-02-01 23:25:01 +0530

    update banner of the tomato and guawa

--- client/src/TomatoGuawa.jsx
+++ client/src/TomatoGuawa.jsx
@@ -12,13 +12,29 @@ function TomatoGuawa() {
       <Header />
       <div className="bg-gray-100">
         {/* Banner */}
-        <div
-          className="flex items-center justify-center w-full h-56 text-white bg-center bg-cover"
-          style={{
-            backgroundImage: `url(${seedsbanner})`,
-          }}
-        >
-          <h1 className="text-4xl font-bold">Fruits Quality Assessment</h1>
+        
+
+        <div className="relative w-full h-56 mb-8">
+          {/* Background Image with Dark Overlay */}
+          <div
+            className="absolute inset-0 bg-center bg-cover"
+            style={{
+              backgroundImage: `url(${seedsbanner})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
+            {/* Dark Overlay */}
+          </div>
+
+          {/* Text Content */}
+          <div className="relative flex items-center justify-center h-full">
+            <h2 className="p-4 text-4xl font-bold text-white">
+              Fruits Quality Assessment
+            </h2>
+          </div>
         </div>
 
         <div className="max-w-screen-xl px-6 mx-auto">
