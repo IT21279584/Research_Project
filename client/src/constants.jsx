447bd8d05d915ef5a2e5b29d04b0004fdcb86037
IT21338748 <it21279584@my.sliit.lk>
2025-02-02 01:00:05 +0530

    Add url constant file

---
+++ client/src/constants.jsx
@@ -0,0 +1,7 @@
+export const BASE_URL_CORN = "http://localhost:5002";
+export const BASE_URL_SOYBEAN = "http://localhost:5000";
+export const BASE_URL_RICE = "http://localhost:5008";
+export const BASE_URL_TOMATO = "http://localhost:5006";
+export const BASE_URL_GUAVA = "http://localhost:5004";
+export const BASE_URL_EGG = "http://localhost:5010";
+export const BASE_URL_USER = "http://localhost:5012";
